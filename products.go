0b16ef75187c38d6029f978a434004c604d8175a
Mykhailo Logvynets <mlogv@softserveinc.com>
2021-01-21 10:05:33 +0200

    filter menu done

--- products.go
+++ products.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"os"
-	"strings"
+	"strconv"
+	// "errors"
+	// "strings"
 )
 
 type Product struct {		//initializing a struct which will hold products
@@ -27,15 +29,20 @@ func MainMenu() {
 		fmt.Println("2. Filter Products")
 		fmt.Println("3. Exit")
 		fmt.Printf("Your choice is: ")
-		n, err := fmt.Scan(&option)
-		fmt.Println(n, err)
-		fmt.Println(option)
-		fmt.Printf("%T\n", option)
+		// fmt.Scan(&option)
+		_, err := fmt.Scan(&option)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// fmt.Println(n, err)
+		// fmt.Println(option)
+		// fmt.Printf("%T\n", option)
 
-		if err != nil || option < 1 || option > 3 {
+		if option < 1 || option > 3 {
 			fmt.Println(`Enter "1" to Show Products, press "2" to Filter them or "3" to Exit`)
 			// MainMenu()
 		}
+		
 	
 		switch option {
 			case 1: 
@@ -58,8 +65,6 @@ func ShowProductsMenu(p []Product) {
 
 	for {
 		var option int
-		// fmt.Println("Show Products Menu:")
-		// fmt.Println(p[0])
 		fmt.Printf("ID-%d\n%s\n%s\nPrice: %d\nSales Price: %d\n%s\n", p[i].ID, p[i].Name, 
 					p[i].Description, p[i].Price, p[i].SalesPrice, p[i].Features)
 		fmt.Println("1. Edit product")
@@ -129,18 +134,16 @@ func EditProductMenu(p *Product) {
 }
 
 func FilterProductMenu(p []Product) {
-	i := 0
+	
 	for {
 		var option int
-		fmt.Printf("ID-%d\n%s\n%s\nPrice: %d\nSales Price: %d\n%s\n", p[i].ID, p[i].Name, 
-		p[i].Description, p[i].Price, p[i].SalesPrice, p[i].Features)
 		fmt.Println("Choose field to filter by:")
 		fmt.Println("1. Name")
-		fmt.Println("2. Description")
-		fmt.Println("3. Price")
-		fmt.Println("4. Sales Price")
-		fmt.Println("5. Features")
-		fmt.Println("6. Back to menu")
+		fmt.Println("2. Price")
+		// fmt.Println("3. Description")
+		// fmt.Println("4. Sales Price")
+		// fmt.Println("5. Features")
+		fmt.Println("3. Back to menu")
 		fmt.Printf("Your choice is: ")
 		fmt.Scan(&option)
 		switch option {
@@ -148,25 +151,76 @@ func FilterProductMenu(p []Product) {
 				var userInput string
 				fmt.Println("Which Name to filter by?")
 				fmt.Scan(&userInput)
-				if strings.Compare(userInput, p[i].Name) == 0 {
-					fmt.Printf("ID-%d\n%s\n%s\nPrice: %d\nSales Price: %d\n%s\n", p[i].ID, p[i].Name, 
-					p[i].Description, p[i].Price, p[i].SalesPrice, p[i].Features)
+				for i, val := range p {
+					if userInput == val.Name {
+						fmt.Printf("ID-%d\n%s\n%s\nPrice: %d\nSales Price: %d\n%s\n", p[i].ID, p[i].Name, 
+						p[i].Description, p[i].Price, p[i].SalesPrice, p[i].Features)
+					} else if userInput != val.Name && i == len(p) - 1 {
+						fmt.Printf("We don't have any item matching %v\n", userInput)
+					}
 				}
 			case 2:  
-				fmt.Println("Description")
-			case 3:  
 				fmt.Println("Price")
+				filterByPrice(p)
+				// FilterProductMenu(products)
+			// case 3:  
+			// 	fmt.Println("Description")
 				
-			case 4:  
-				fmt.Println("Sales Price")
-			case 5:  
-				fmt.Println("Features")
-			case 6:  
+			// case 4:  
+			// 	fmt.Println("Sales Price")
+			// case 5:  
+			// 	fmt.Println("Features")
+			case 3:  
 				MainMenu()
 			default:
 				fmt.Println("Press one of available options!")
 		}
-		i++
+	}
+}
+
+func filterByPrice(p []Product) {
+	// var err1, err2 error
+	// err1 := errors.New("Flag UP")
+	// err2 := errors.New("Flag UP")
+	// fmt.Println(err1, err2)
+
+	for {
+		var minPrice string
+		var maxPrice string
+		fmt.Println("What is the minimum item Price to filter by?")
+		fmt.Scan(&minPrice)
+		min, err1 := strconv.Atoi(minPrice)
+		// fmt.Println(err1)
+		if err1 != nil {
+			fmt.Println("Nunbers only")
+			// fmt.Println("Please enter minimum price to look up from")
+			// fmt.Scan(&minPrice)
+			continue
+		}
+		// fmt.Printf("%d\n", min)
+		// fmt.Printf("%T\n", min)
+
+		fmt.Println("What is the maximum item Price to filter by?")
+		fmt.Scan(&maxPrice)
+		max, err2 := strconv.Atoi(maxPrice)
+		// fmt.Println(err2)
+		if err2 != nil {
+			fmt.Println("Nunbers only")
+			// fmt.Println("Please enter maximum price to look up from")
+			continue
+		}
+		// fmt.Printf("%d\n", max)
+		// fmt.Printf("%T\n", max)
+
+		for i, val := range p {
+			if min <= val.Price && val.Price <= max {
+				fmt.Printf("ID-%d\n%s\n%s\nPrice: %d\nSales Price: %d\n%s\n", p[i].ID, p[i].Name, 
+				p[i].Description, p[i].Price, p[i].SalesPrice, p[i].Features)
+			} 
+		}
+		if err1 == nil && err2 == nil {
+			break
+		}
 	}
 }
 
